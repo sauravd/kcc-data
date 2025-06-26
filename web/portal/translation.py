4a33a353dc20fb64f5af95e085e5fc82939a92d6
sauravd <sauravdahal@gmail.com>
2025-06-26 12:17:23 +0300

    feat: added all the modified files

---
+++ web/portal/translation.py
@@ -0,0 +1,23 @@
+from modeltranslation.translator import register, TranslationOptions
+from .models import PortalCard, AboutPage, PortalSetting
+
+@register(PortalCard)
+class PortalCardTranslation(TranslationOptions):
+    fields = ('title', 'description',)
+
+@register(AboutPage)
+class AboutPageTranslation(TranslationOptions):
+    fields = ('content',)
+
+
+@register(PortalSetting)
+class PortalSettingTranslation(TranslationOptions):
+    fields = (
+        'site_title',
+        'organization',
+        # If you’d also like your login/logout link text or captions translatable:
+        # 'login_text',
+        # 'logout_text',
+    )
+
+    
